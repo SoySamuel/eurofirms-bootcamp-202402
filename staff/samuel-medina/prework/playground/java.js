0aa013aa40ac0ae4d1bc8edadd7938084ea2e495
Samuel Medina <aroqsi@gmail.com>
2024-02-23 17:51:08 +0100

    addprework files #85

---
+++ staff/samuel-medina/prework/playground/java.js
@@ -0,0 +1,10 @@
+function test(a, b) {
+    if (a<b){console.log ("viva")
+        return "yes"
+    }
+    return a + b
+}
+
+console.log(test 4,5)
+var test = test(2, 3)
+
---
+++ staff/samuel-medina/prework/playground/test.js
@@ -0,0 +1,17 @@
+var numbers = {
+    "0": 20,
+    "1": 30,
+    "2": 40,
+    "3": 50,
+    length: 4
+}
+
+numbers.push( { 
+"ID" : id, 
+"Name" : name, 
+"Notes" : notes } 
+);
+
+console.log(numbers)
+
+
